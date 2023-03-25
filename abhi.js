05f8c5e6236eb176e89d4a4f46dbc62c8fcfb9e1
ishtuu <guptaishita242@gmail.com>
2023-03-25 22:01:30 +0530

    first commit

---
+++ abhi.js
@@ -0,0 +1,8 @@
+// typing effect
+let typed = new Typed (".profile-input",{
+    strings: ['Software Tester Engineer', "Quality Analyst Engineer", "Quality Control Engineer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+})
